7b97b6c4d5f1341a797854b084e4b7f67b158e10
vladimir2809 <46346862+vladimir2809@users.noreply.github.com>
2022-07-03 19:39:23 +0500

    Первая рабочая версия. Но сырая.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 var context;
 var canvas;
+var screenWidth = 800;
+var screenHeight = 600;
 var timeNow = 0;
 //var SRBRed=0;// выбор приема Blue select reception blue
+var maxHpAndEnergy = 1000;
+var HPBlue = maxHpAndEnergy;
+var HPRed = maxHpAndEnergy ;
+var energyBlue = maxHpAndEnergy;
+var energyRed = maxHpAndEnergy;
+var gameOver = 0;// если 0 - игра продолжается, 1 - победил синий,2 - победил красный 
 window.addEventListener('load', function () {
     preload();
+    setInterval(update,16); 
     create();
     setInterval(drawAll,16);
-    setInterval(update,16); 
 });
 var frameArr={
     
@@ -34,15 +42,21 @@ var Line={
         10,
     ],
 } */
-var humanBlue={
+var humanBlue;
+var humanRed;
+var Human={
+    name:'',
     x:100,
     y:263,
     xBuffer:100,
     xStart:100,
+    hitMade:0,// УДАР ПРОИЗВЕДЕН 
+    timeHitMe: 0,
 	dx:0,
 	dy:0,
-	numMoveFrame:null,
-    numAction:0,
+    speed: 100,
+    downHP: 0,
+    downEnergy:0,
     selectFrame:0,
     SR: 0,// выбор приема
     time:0,
@@ -64,36 +78,42 @@ var humanBlue={
     lineArr:[],
     
 }
-var humanRed={
-    x:500,
-    y:263,
-    xBuffer:500,
-    xStart:500,
-	dx:0,
-	dy:0,
-	numMoveFrame:null,
-    numAction:0,
-    selectFrame:0,
-    SR: 0,// выбор приема
-    time:0,
-    xTime:0,
-    timeNow:0,
-    angleArr:[
-        -90,
-        45,
-        135,
-        90,
-        90,
-        45,
-        135,
-        -15,
-        -15,
-        -90
-    ],
+//var bigText = {
+//    being:false,
+//    x:null,
+//    y:null,
+//    text:'',
+//}
+//var humanRed={
+//    x:500,
+//    y:263,
+//    xBuffer:500,
+//    xStart:500,
+//	dx:0,
+//	dy:0,
+//	numMoveFrame:null,
+//    numAction:0,
+//    selectFrame:0,
+//    SR: 0,// выбор приема
+//    time:0,
+//    xTime:0,
+//    timeNow:0,
+//    angleArr:[
+//        -90,
+//        45,
+//        135,
+//        90,
+//        90,
+//        45,
+//        135,
+//        -15,
+//        -15,
+//        -90
+//    ],
 
-    lineArr:[],
+//    lineArr:[],
     
-}
+//}
 function preload()
 {
     
@@ -105,10 +125,19 @@ function create()
     initKeyboardAndMouse(['ArrowLeft','Space','ArrowRight',
                             'ArrowUp','ArrowDown', 'ControlLeft',"KeyW"
                             ,"KeyD","KeyS","KeyA"]);
-    humanBlue.lineArr=calcArrLine(10,10/*humanBlue.x,humanBlue.y*/,humanBlue.angleArr);
+    humanBlue=JSON.parse(JSON.stringify(Human));
+    humanRed=JSON.parse(JSON.stringify(Human));
+    humanRed.x = 300;
+    humanBlue.name='Blue';
+    humanRed.name='Red';
+    humanBlue.lineArr=calcArrLine(humanBlue.x,humanBlue.y,humanBlue.angleArr);
     humanRed.lineArr=calcArrLine(humanRed.x,humanRed.y,humanRed.angleArr);
     humanBlue.xStart = humanBlue.xBuffer = actionBlue[0][0].xHuman;
     humanRed.xStart = humanRed.xBuffer = actionRed[0][0].xHuman;
+    HPBlue = maxHpAndEnergy;//* 0.1;
+    HPRed = maxHpAndEnergy;//* 0.1;
+    energyBlue = maxHpAndEnergy;
+    energyRed = maxHpAndEnergy;
 }
 function calcLineInHuman(x,y,angle,length)// добавить линию 
 {
@@ -175,31 +204,36 @@ function drawAll()
     context.moveTo(1,canvas.height/2);
     context.lineTo(canvas.width,canvas.height/2 );
     context.stroke();
-    //context.strokeStyle='rgb(0,0,255)';
     drawHuman(humanBlue,'Blue');
-    drawHuman(humanRed,"Red");
-    //for (let i=0;i<humanBlue.lineArr.length;i++)
-    //{
-    //    let x=humanBlue.lineArr[i].x;
-    //    let y=humanBlue.lineArr[i].y;
-    //    let x1=humanBlue.lineArr[i].x1;
-    //    let y1=humanBlue.lineArr[i].y1;
-    //    context.beginPath();
-    //    context.moveTo(x,y);
-    //    context.lineTo(x1,y1 );
-    //    context.stroke();
-    //    if (i==humanBlue.lineArr.length-1)//рисуем голову
-    //    { 
-    //        let sizeHead=7;
-    //        let xx=Math.cos(pi*(humanBlue.angleArr[i])/180)*(sizeHead+humanBlue.lineArr[i].length)+
-    //                                    humanBlue.lineArr[i].x;
-    //        let yy=Math.sin(pi*(humanBlue.angleArr[i])/180)*(sizeHead+humanBlue.lineArr[i].length)+
-    //                                    humanBlue.lineArr[i].y;                    
-    //        context.beginPath()
-    //        context.arc(xx,yy, sizeHead,0,2*pi);
-    //        context.stroke();
-    //    }
-    //}
+    drawHuman(humanRed, "Red");
+    //if (HPBlue >= 0) HPBlue -= 5.9; else HPBlue = maxHpAndEnergy;
+    //if (energyBlue >= 0) energyBlue -= 5.9; else energyBlue= maxHpAndEnergy;
+    drawStrip(30, 30,  maxHpAndEnergy, maxHpAndEnergy, 0, 'Red');
+    drawStrip(30, 30, HPBlue, maxHpAndEnergy, 0, 'Green');
+    drawStrip(screenWidth-screenWidth * 0.4 - 30, 30,  maxHpAndEnergy, maxHpAndEnergy, 0, 'Red');
+    drawStrip(screenWidth-screenWidth * 0.4 - 30, 30, HPRed, maxHpAndEnergy, 1, 'Green');
+    drawStrip(30, 7, energyBlue, maxHpAndEnergy, 0, 'Blue');
+    drawStrip(screenWidth-screenWidth * 0.4 - 30, 7, energyRed, maxHpAndEnergy, 1, 'Blue');
+    if (gameOver!=0) 
+    {
+        let text = gameOver == 1 ? 'Победил синий' : 'Победил красный';
+        let color = gameOver == 1 ? "Blue" : "Red";
+        drawTextCenterScreen(text, "Arial", 40, color,screenHeight/2-30);
+    }
+}
+function drawStrip(x,y,value,max,side,color)
+{
+    context.fillStyle=color;
+    let height = 20;
+    let width = screenWidth * 0.4;
+    if (side==0)
+    {
+        if (max>0 && value>0) context.fillRect(x,y,width*(value/max),height);
+    }
+    if (side==1)
+    {
+        if (max > 0 && value>0) context.fillRect(x+(width-(width * (value/max))), y, width * (value/max ), height);
+    }
 }
 function drawHuman(human,color)
 {
@@ -227,34 +261,142 @@ function drawHuman(human,color)
         }
     }
 }
+function drawTextCenterScreen(text,font,fontSize,color='rgb(255,128,0)',yText=null)// нарисоваать текст по середине экрана
+{
+    context.fillStyle=color;
+    let heightText=fontSize;
+    context.beginPath();
+    context.font = fontSize+'px '+font;;
+    let metrics = context.measureText(text);
+    
+    let x=1;
+    let y=1;
+    let width=screenWidth;
+   // context.strokeRect(this.widthTab*i,this.y,this.widthTab,20);
+    context.fillText(text, x + width / 2 - metrics.width / 2, yText == null ? y + screenHeight / 2 + fontSize / 3 : yText);
+}
 function update()
 {
 	 //updateLineHuman(100,263,1);
 	timeNow=new Date().getTime();
-	if (checkPressKey('ArrowRight') && humanBlue.SR!=1)
-	{
-		humanBlue.SR=1;
-	}
-	if (checkPressKey('ArrowLeft') && humanBlue.SR!=2)
-	{
-		humanBlue.SR=2;
-	}
-	if (checkPressKey('KeyS') && humanBlue.SR!=3)
-	{
-		humanBlue.SR=3;
-	}
-        if (checkPressKey('KeyA') && humanBlue.SR!=4)
-	{
-		humanBlue.SR=4;
-	}
-        if (checkPressKey('KeyD') && humanBlue.SR!=5)
-	{
-		humanBlue.SR=5;
-	}
-    if(humanRed.SR==0)humanRed.SR =randomInteger(0,100) < 10 ? randomInteger(1,5): 0;
-  
-    updateHuman(humanBlue, actionBlue);
-    updateHuman(humanRed, actionRed);
+    if (gameOver==0)
+    {
+        let dist = 35;
+	    if (checkPressKey('ArrowRight') && humanBlue.SR!= 1&& humanBlue.x<humanRed.x-dist)
+	    {
+		    humanBlue.SR=1;
+	    }
+	    if (checkPressKey('ArrowLeft') && humanBlue.SR!=2 && humanBlue.x>40)
+	    {
+		    humanBlue.SR=2;
+	    }
+	    if (checkPressKey('KeyS') && humanBlue.SR!=3)
+	    {
+		    humanBlue.SR=3;
+            energyBlue -= 5;
+	    }
+            if (checkPressKey('KeyA') && humanBlue.SR!=4)
+	    {
+		    humanBlue.SR=4;
+            energyBlue -= 7;
+	    }
+            if (checkPressKey('KeyD') && humanBlue.SR!=5)
+	    {
+		    humanBlue.SR=5;
+            energyBlue -= 10;
+	    }
+        if (humanRed.SR == 0)
+        {
+            // randomInteger(0,100) < 10 ? randomInteger(3,5): 0;
+            if (energyRed>maxHpAndEnergy*0.5)   humanRed.SR = 1; else humanRed.SR = 2;
+        }
+        if (humanRed.SR == 1 && humanRed.x < humanBlue.x + dist) humanRed.SR = 0;
+        if (humanRed.SR == 2 && humanRed.x >screenWidth-dist) humanRed.SR = 0;
+        let rand = 0;
+        if (humanRed.SR == 0 && humanRed.x < humanBlue.x + dist * 1.5) // условия атаки красным
+        {
+            rand = randomInteger(0, 100) < 10 ? randomInteger(3, 5) : 0
+            if (rand==3)
+            {
+                energyRed -= 5;
+            }
+            if (rand==4)
+            {
+                energyRed -= 7;
+            }
+            if (rand==5)
+            {
+                energyRed -= 10;
+            }
+            humanRed.SR = rand;
+        }
+        updateHuman(humanBlue, actionBlue);
+        updateHuman(humanRed, actionRed);
+        let mult = 1.5;
+        if (humanRed.hitMade==0)
+        {
+            if (humanBlue.SR == 3 && humanBlue.selectFrame == 2 && humanRed.x - humanBlue.x < dist * mult) 
+            {
+                humanRed.downHP = 20; 
+                humanRed.hitMade = 1;
+            }
+            if (humanBlue.SR == 4 && humanBlue.selectFrame == 2 && humanRed.x - humanBlue.x < dist * mult) 
+            {
+                humanRed.downHP = 10;
+                if (energyRed > 10) humanRed.downEnergy = 20;
+                humanRed.hitMade = 1;
+            }
+            if (humanBlue.SR == 5 && humanBlue.selectFrame == 2 && humanRed.x - humanBlue.x < dist * mult) 
+            {
+                humanRed.downHP = 50;
+                humanRed.hitMade = 1;
+            }  
+        }
+        if(humanBlue.hitMade==0)
+        {
+            if (humanRed.SR == 3 && humanRed.selectFrame == 2 && humanBlue.x - humanRed.x < dist * mult) 
+            {
+                humanBlue.downHP = 20;
+                humanBlue.hitMade = 1;
+            }
+            if (humanRed.SR == 4 && humanRed.selectFrame == 2 && humanBlue.x - humanRed.x < dist * mult) 
+            {
+                humanBlue.downHP = 10; 
+                if (energyBlue > 10)humanBlue.downEnergy = 20;
+                humanBlue.hitMade = 1;
+            }
+            if (humanRed.SR == 5 && humanRed.selectFrame == 2 && humanBlue.x - humanRed.x < dist * mult) 
+            {
+                humanBlue.downHP = 50;
+                humanBlue.hitMade = 1;
+            }
+        } 
+        let mult2 = 0.5;
+        if (humanBlue.SR==0)
+        {
+            if (energyBlue < maxHpAndEnergy) energyBlue += 1 * mult2; else energyBlue = maxHpAndEnergy;
+        }
+        if (humanRed.SR==0)
+        {
+            if (energyRed < maxHpAndEnergy) energyRed += 1 * mult2; else energyRed = maxHpAndEnergy;
+        }
+        //console.log(humanBlue.hitMade);
+        if (humanBlue.SR == 0 && humanBlue.selectFrame == 0) humanRed.hitMade = 0;
+        if (humanRed.SR == 0 && humanRed.selectFrame == 0) humanBlue.hitMade = 0;
+        if (HPBlue <= 0) gameOver = 2;
+        if (HPRed <= 0) gameOver = 1;
+    }
+    if (gameOver!=0)
+    {
+        if (checkPressKey('Space')==true)
+        {
+            gameOver = 0;
+            create();
+        }
+    }
+    //if (humanRed.hitMade==2) humanRed.hitMade = 0;
+    //if (humanBlue.hitMade==2) humanBlue.hitMade = 0;
+
  }
 function updateHuman(human,actionList)
 {
@@ -273,41 +415,59 @@ function updateHuman(human,actionList)
         //console.log("dx "+human.dx + ' SR ' + human.SR +" sF " +human.selectFrame);
         if (human.xBuffer!=0)
         {
-      
-                    
             human.dx =human.xBuffer/5;
          //   console.log(human.dx+'  '+human.selectFrame);
-          
             human.xBuffer=0;
         }
         else
         {
             human.dx = 0;       
-            
-
         }
-            
-
     }
     else
     {
         human.dx = 0;
     }
+    let summSpeed = 0;
+    let mult = 0.25;
+    let add = 30;
+    if (human.name == 'Red') human.speed = add + energyRed * mult;
+    if (human.name == 'Blue') human.speed = add + energyBlue * mult;
 
+    summSpeed = add + maxHpAndEnergy * mult;
     arrElemCopy(human.angleArr,actionList[human.SR][human.selectFrame].angleArr);
+	human.lineArr=calcArrLine(Math.trunc(human.x),human.y,human.angleArr);
     human.timeNow=new Date().getTime();
-    if (human.timeNow-human.xTime> 1 )
+	if (human.timeNow-human.xTime> (summSpeed*5)/(human.speed+1) )
 	{
         human.x += human.dx;
         human.xTime = new Date().getTime();;
     }
-	human.lineArr=calcArrLine(Math.trunc(human.x),human.y,human.angleArr);
-	
-    if (human.timeNow-human.time> 100 )
+    if (human.timeNow-human.time> (summSpeed*50)/(human.speed+1) )
 	{ 
 		human.selectFrame++;
+    
 	    human.time=new Date().getTime();
 
-	};
+	}
+    if ((human.downHP>0 || human.downEnergy>0) && human.hitMade==1)
+    {
+        if (human.name=='Red')
+        {
+            HPRed -= human.downHP;
+            energyRed -= human.downEnergy;
+            human.downHP = 0;
+            human.downEnergy = 0;
+        }
+        if (human.name=='Blue')
+        {
+            HPBlue -= human.downHP;
+            energyBlue -= human.downEnergy;
+            human.downHP = 0;
+            human.downEnergy = 0;
+        }
+        human.hitMade = 2; //alert(585);
+        human.timeHitMe = new Date().getTime();;
+    }
 }
 
